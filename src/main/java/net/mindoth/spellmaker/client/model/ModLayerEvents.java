0ab85207bf974bf66d95ed43195141cab577505f
Mindoth <Mindoth@gmail.com>
2025-11-07 21:51:21 +0200

    Holy spaghetti, the armor renders

--- src/main/java/net/mindoth/spellmaker/client/model/ModLayerEvents.java
+++ src/main/java/net/mindoth/spellmaker/client/model/ModLayerEvents.java
@@ -3,47 +3,63 @@ package net.mindoth.spellmaker.client.model;
 import com.google.common.collect.Lists;
 import net.mindoth.spellmaker.SpellMaker;
 import net.mindoth.spellmaker.item.armor.ModArmorItem;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.renderer.entity.ArmorModelSet;
-import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.resources.model.EquipmentClientInfo;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.ClientHooks;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 @EventBusSubscriber(modid = SpellMaker.MOD_ID, value = Dist.CLIENT)
 public class ModLayerEvents {
 
-    public static final ModelLayerLocation SIMPLE_ROBE_LAYER = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(SpellMaker.MOD_ID, "simple_robe"), "main");
+    public static final ArmorModelSet<ModelLayerLocation> SIMPLE_ROBE = registerArmorSet("simple_robe");
 
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
-        event.registerLayerDefinition(SIMPLE_ROBE_LAYER, SimpleRobeModel::createLayer);
+        event.registerLayerDefinition(SIMPLE_ROBE.head(), SimpleRobeModel::createHeadLayer);
+        event.registerLayerDefinition(SIMPLE_ROBE.chest(), SimpleRobeModel::createBodyLayer);
+        event.registerLayerDefinition(SIMPLE_ROBE.legs(), SimpleRobeModel::createLegsLayer);
+        event.registerLayerDefinition(SIMPLE_ROBE.feet(), SimpleRobeModel::createBootsLayer);
     }
 
-
-
-    public static SimpleRobeModel<HumanoidRenderState> SIMPLE_ROBE_MODEL = null;
-
     @SubscribeEvent
-    public static void registerModels(EntityRenderersEvent.AddLayers event) {
-        SIMPLE_ROBE_MODEL = new SimpleRobeModel<>(Minecraft.getInstance().getEntityModels().bakeLayer(SIMPLE_ROBE_LAYER));
-    }
-
-
-
-    @SubscribeEvent
-    public static void registerItemExtensions(RegisterClientExtensionsEvent event) {
+    public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
+        IClientItemExtensions armor = new IClientItemExtensions() {
+            @Override
+            @NotNull
+            public Model getHumanoidArmorModel(@NotNull ItemStack stack, @NotNull EquipmentClientInfo.LayerType type, @NotNull Model original) {
+                if ( original instanceof HumanoidModel<?> humanoidModel && stack.getItem() instanceof ModArmorItem item ) {
+                    HumanoidModel<?> modArmor = new SimpleRobeModel(SimpleRobeModel.createLayerByType(item.type).bakeRoot());
+                    ClientHooks.copyModelProperties(humanoidModel, modArmor);
+                    return modArmor;
+                }
+                else return original;
+            }
+        };
         List<Item> items = Lists.newArrayList();
         for ( Item item : BuiltInRegistries.ITEM ) if ( item instanceof ModArmorItem ) items.add(item);
-        event.registerItem(ModItemExtension.INSTANCE, items.toArray(new Item[0]));
+        event.registerItem(armor, items.toArray(new Item[0]));
+    }
+
+    public static ArmorModelSet<ModelLayerLocation> registerArmorSet(String path) {
+        return new ArmorModelSet(registerModel(path, "helmet"), registerModel(path, "chestplate"), registerModel(path, "leggings"), registerModel(path, "boots"));
+    }
+
+    public static ModelLayerLocation registerModel(String path, String model) {
+        return new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(SpellMaker.MOD_ID, path), model);
     }
 }
--- src/main/java/net/mindoth/spellmaker/client/model/SimpleRobeModel.java
+++ src/main/java/net/mindoth/spellmaker/client/model/SimpleRobeModel.java
@@ -4,18 +4,64 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.renderer.entity.ArmorModelSet;
+import net.minecraft.world.item.equipment.ArmorType;
 
-public class SimpleRobeModel<T extends HumanoidRenderState> extends HumanoidModel<T> {
+import java.util.Set;
+import java.util.function.Function;
+
+public class SimpleRobeModel extends HumanoidModel {
 
     //TODO: Armor "breathes" on an armorstand
     public SimpleRobeModel(ModelPart root) {
         super(root);
     }
 
-    public static LayerDefinition createLayer() {
+    public static ArmorModelSet<LayerDefinition> SIMPLE_ROBE_ARMOR_LAYER =
+            SimpleRobeModel.createArmorSet().map(mesh -> LayerDefinition.create(mesh, 64, 64));
+
+    public static LayerDefinition createLayerByType(ArmorType type) {
+        if ( type == ArmorType.HELMET ) return createHeadLayer();
+        if ( type == ArmorType.CHESTPLATE ) return createBodyLayer();
+        if ( type == ArmorType.LEGGINGS ) return createLegsLayer();
+        if ( type == ArmorType.BOOTS ) return createBootsLayer();
+        return null;
+    }
+
+    public static LayerDefinition createHeadLayer() {
+        return SIMPLE_ROBE_ARMOR_LAYER.head();
+    }
+
+    public static LayerDefinition createBodyLayer() {
+        return SIMPLE_ROBE_ARMOR_LAYER.chest();
+    }
+
+    public static LayerDefinition createLegsLayer() {
+        return SIMPLE_ROBE_ARMOR_LAYER.legs();
+    }
+
+    public static LayerDefinition createBootsLayer() {
+        return SIMPLE_ROBE_ARMOR_LAYER.feet();
+    }
+
+    public static ArmorModelSet<MeshDefinition> createArmorSet() {
+        return createArmorSet(SimpleRobeModel::createBaseArmor);
+    }
+
+    public static ArmorModelSet<MeshDefinition> createArmorSet(Function<CubeDeformation, MeshDefinition> base) {
+        MeshDefinition head = base.apply(new CubeDeformation(1.0F));
+        head.getRoot().retainPartsAndChildren(Set.of("head"));
+        MeshDefinition body = base.apply(new CubeDeformation(1.0F));
+        body.getRoot().retainExactParts(Set.of("body", "left_arm", "right_arm"));
+        MeshDefinition legs = base.apply(new CubeDeformation(1.0F));
+        legs.getRoot().retainExactParts(Set.of("left_leg", "right_leg", "body"));
+        MeshDefinition boots = base.apply(new CubeDeformation(1.0F));
+        boots.getRoot().retainExactParts(Set.of("left_leg", "right_leg"));
+        return new ArmorModelSet(head, body, legs, boots);
+    }
+
+    public static MeshDefinition createBaseArmor(CubeDeformation scale) {
         float deform = 1.0F;
-        CubeDeformation scale = new CubeDeformation(deform);
         MeshDefinition mesh = HumanoidModel.createMesh(scale, 0);
         PartDefinition root = mesh.getRoot();
 
@@ -39,6 +85,7 @@ public class SimpleRobeModel<T extends HumanoidRenderState> extends HumanoidMode
                 .addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F,
                         new CubeDeformation(deform)), PartPose.offset(1.9F, 12.0F, 0.0F));
 
-        return LayerDefinition.create(mesh, 64, 64);
+        return mesh;
+        //return LayerDefinition.create(mesh, 64, 64);
     }
 }
