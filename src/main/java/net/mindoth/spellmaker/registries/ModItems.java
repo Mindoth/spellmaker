1a6be2144aed0db67c49d903aa523647c22de799
Mindoth <Mindoth@gmail.com>
2025-11-21 04:47:09 +0200

    Added Arcane Staff

--- src/main/java/net/mindoth/spellmaker/registries/ModItems.java
+++ src/main/java/net/mindoth/spellmaker/registries/ModItems.java
@@ -70,6 +70,9 @@ public class ModItems {
     public static final DeferredItem<Item> WOODEN_STAFF = ITEMS.registerItem("wooden_staff",
             (properties) -> new StaffItem(properties.durability(128)));
 
+    public static final DeferredItem<Item> ARCANE_STAFF = ITEMS.registerItem("arcane_staff",
+            (properties) -> new StaffItem(properties.durability(512)));
+
     public static final DeferredItem<Item> WOOL_ROBE_HOOD = ITEMS.registerItem("wool_robe_hood",
             (properties) -> new WoolRobeItem(properties, ModArmorMaterials.WOOL_ROBE_MATERIAL, ArmorType.HELMET, withMagickAttributes(15, 0.05D)));
 
